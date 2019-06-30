76099df9edf0eb40b2d5d7c2fe090b06db3284ff
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2019-06-30 13:46:50 -0400

    Updated bottom half of main menu <nav>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,9 @@
 console.log('\'Allo \'Allo!');
 
 // Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+"https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere"
+$(function () { $('[data-toggle="tooltip"]').tooltip(); });
 
 // Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+"https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere"
+$(function () { $('[data-toggle="popover"]').popover(); });
